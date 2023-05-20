72d565e84e67bedd887d41a71fc271ad6a329d3a
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-05-20 15:48:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,11 +1,23 @@
 
+import os
+import logging
+import random
+import asyncio
 from Script import script
-from info import LOG_CHANNEL
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
+from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, get_bad_files
+from database.users_chats_db import db
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHNL_LNK, GRP_LNK, REQST_CHANNEL, SUPPORT_CHAT_ID, MAX_B_TN, IS_VERIFY
+from utils import get_settings, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, send_all
+from database.connections_mdb import active_connection
+import re
+import json
+import base64
 
 
-@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command("startt") & ~filters.private & ~filters.channel)
 async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
